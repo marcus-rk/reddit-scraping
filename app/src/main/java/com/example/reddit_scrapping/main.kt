5f0d542c05f78a756e680afa9e0ecd42ce856cd3
marcus_rk <makj0005@stud.kea.dk>
2024-03-23 19:35:40 +0100

    Added better variable names and comments

--- app/src/main/java/com/example/reddit_scrapping/main.kt
+++ app/src/main/java/com/example/reddit_scrapping/main.kt
@@ -35,17 +35,18 @@ fun main() {
 fun getRedditPostComments(driver: ChromeDriver, url: String): MutableList<String> {
     driver.get(url)
 
-    // Get all comment div's
-    val commentWebElements: List<WebElement> = driver.findElements(By.id("-post-rtjson-content"));
+    // Get all comment Divs
+    val commentDivWebElements: List<WebElement> = driver.findElements(By.id("-post-rtjson-content"));
 
     // Add every comment to a list
     val comments: MutableList<String> = mutableListOf()
-    for (commentWebElement in commentWebElements) {
-        // Comment paragraph/paragraphs
-        val commentWebChildElements: List<WebElement> = commentWebElement.findElements(By.tagName("p"))
+
+    for (commentWebElement in commentDivWebElements) {
+        // Comment paragraph/paragraphs element
+        val commentDivWebChildElements: List<WebElement> = commentWebElement.findElements(By.tagName("p"))
 
         var comment: String = "";
-        for (paragraphElement in commentWebChildElements) {
+        for (paragraphElement in commentDivWebChildElements) {
             comment += paragraphElement.text;
         }
         comments.add(comment)
@@ -59,11 +60,13 @@ fun createAndWriteToExcelFile(comments: MutableList<String>, fileName: String): 
     val xlWb = XSSFWorkbook()
     val xlWs = xlWb.createSheet()
 
+    // Add one comment for each row
     for (i in comments.indices){
         val row: Row = xlWs.createRow(i);
         row.createCell(0).setCellValue(comments[i]);
     }
 
+    // Create excel file add rows via xlWs
     val outputStream = FileOutputStream("app/src/main/java/com/example/reddit_scrapping/${fileName}.xlsx")
     xlWb.write(outputStream)
     xlWb.close()
