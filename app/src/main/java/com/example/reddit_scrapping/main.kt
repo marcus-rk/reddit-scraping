930e73e35999a13106e5196e6068b23fd4a7e90f
marcus_rk <makj0005@stud.kea.dk>
2024-03-23 18:39:23 +0100

    Started working on scraping

--- app/src/main/java/com/example/reddit_scrapping/main.kt
+++ app/src/main/java/com/example/reddit_scrapping/main.kt
@@ -1,5 +1,28 @@
 package com.example.reddit_scrapping
 
+import org.openqa.selenium.By
+import org.openqa.selenium.WebElement
+import org.openqa.selenium.chrome.ChromeDriver
+
 fun main() {
-    println("Hello World!")
+    // It’s you, you creeper. Scraping
+    val driver = ChromeDriver();
+    driver.get("https://www.reddit.com/r/lonely/comments/12fzic8/its_you_you_creeper/")
+
+    // Get all comment div's
+    val commentWebElements: List<WebElement> = driver.findElements(By.idName("-post-rtjson-content"));
+
+    // Add every comment to a list
+    val comments: MutableList<String> = mutableListOf()
+    for (commentWebElement in commentWebElements) {
+        var comment: String = "";
+        for (paragraph in commentWebElement) {
+            comment += paragraph.text;
+        }
+        comments.add(comment)
+    }
+
+    // Console.log result:
+    for (comment in comments)
+        println(comment)
 }
